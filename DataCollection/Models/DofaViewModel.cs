528a7b54aeae37ae79e27d072bc958ca44b44bf7
dhavalpatel13 <dhavalpatel13@outlook.com>
2018-10-13 03:23:02 +0530

    commit 3.2

--- DataCollection/Models/DofaViewModel.cs
+++ DataCollection/Models/DofaViewModel.cs
@@ -7,6 +7,8 @@ using DataAccess.Repository;
 using DataCollection.ManageSession;
 using DataCollection.FormService;
 using Newtonsoft.Json;
+using System.Data;
+using System.Reflection;
 
 namespace DataCollection.Models
 {
@@ -17,46 +19,48 @@ namespace DataCollection.Models
         public int DataStatus { get; set; }
         public string DataStatusName { get; set; }
 
-        public void GetDOFAData(int dataCaptYM, string MenuID)
+        public void GetDOFAData(int _dataCaptYM, string MenuID)
         {
             FormsRepository formsRepository = new FormsRepository();
-            dataCaptYM = SessionManager.DataCaptYR > 0 ? SessionManager.DataCaptYR : (dataCaptYM > 0 ? dataCaptYM : 0);
-            if (MenuID == DataAccess.Enum.Menu.DOFA.ToString())
+            DataCaptYM = SessionManager.DataCaptYR > 0 ? SessionManager.DataCaptYR : (_dataCaptYM > 0 ? _dataCaptYM : 0);
+
+            this.DofaData = formsRepository.GetDOFAFormDataByID(DataCaptYM, SessionManager.DeptID);
+
+
+            if (DofaData == null || DofaData.Count == 0)
             {
-                this.DofaData = formsRepository.GetDOFAFormDataByID(DataCaptYM, SessionManager.DeptID, MenuID);
-
-                DataCaptYM = dataCaptYM;
-
-                if (DofaData == null || DofaData.Count == 0)
-                {
-                    DofaData = new List<DofaInfo>();
-                    DofaData.Add(new DofaInfo());
-                    DataStatus = (int)DataAccess.Enum.DataStatus.DataEntryStartedbyOperator;
-                    DataStatusName = "Data Entry Started by Operator";
-                }
-                else
-                {
-                    DataStatus = DofaData.First().DataStatus;
-                    DataStatusName = DofaData.First().DataStatusName;
-                }
+                DofaData = new List<DofaInfo>();
+                DofaData.Add(new DofaInfo());
+                DataStatus = (int)DataAccess.Enum.DataStatus.DataEntryStartedbyOperator;
+                DataStatusName = "Data Entry Started by Operator";
+            }
+            else
+            {
+                DataStatus = DofaData.First().DataStatus;
+                DataStatusName = DofaData.First().DataStatusName;
             }
         }
 
         public bool SaveDOFAData(string action)
         {
-            if(DofaData==null || DofaData.Count ==0)
+            bool success = false;
+            if (DofaData==null || DofaData.Count ==0)
             {
-                return false;
+                return success;
             }
 
             if (DofaData.GroupBy(n => n.empNo).Any(c => c.Count() > 1))
             {
-                return false;
+                return success;
             }
 
             foreach (var item in DofaData)
             {
+                item.MenuID = DataAccess.Enum.Menu.DOFA.ToString();
+                item.DeptID = SessionManager.DeptID;
+                item.DataUser = SessionManager.UserName;
                 item.DataUpdatedOn = DateTime.Now;
+                item.DataCaptYM = SessionManager.DataCaptYR;
                 item.DataStatus = (int)DataAccess.Enum.DataStatus.DataEntryStartedbyOperator;
                 if (action == "Finalize")
                 {
@@ -66,20 +70,54 @@ namespace DataCollection.Models
                 {
                     item.DataStatus = (int)DataAccess.Enum.DataStatus.DataCheckingCompletedbyHOD;
                 }
-
-                item.DataUser = SessionManager.UserName;
-                item.DeptID = SessionManager.DeptID;
                 item.DataValid = "Y";
                 item.DataLocked = "N";
                 item.DataStatusLog = SessionManager.UserName + " " + DateTime.Now.ToString("ddd, dd MMM yyyy HH:mm:ss");
-                item.MenuID = DataAccess.Enum.Menu.DOFA.ToString();
 
             }
-            return true;
+
+            try
+            {
+                FormsRepository formsRepository = new FormsRepository();
+                DataTable dt = ToDataTable(DofaData);
+                success = formsRepository.UpdateBulkDOFAFormData(dt, SessionManager.DataCaptYR, SessionManager.DeptID);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return success;
+        }
+
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Defining type of data column gives proper data table 
+                var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name, type);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
         }
     }
 
-    public class DOfaRequestViewModel
+    public class DofaRequestViewModel
     {
         public DofaViewModel formData { get; set; }
         public string action { get; set; }
