4defbd1cc1e19a2c346fc16b5ca9649bbb906a60
dhavalpatel13 <dhavalpatel13@outlook.com>
2018-10-23 00:41:49 +0530

    Report Header Added.

--- DataCollection/Models/ReportsViewModel.cs
+++ DataCollection/Models/ReportsViewModel.cs
@@ -38,7 +38,7 @@ namespace DataCollection.Models
             return sb;
         }
 
-        public static void GenerateExcelFromDataTable(string sheetName,DataSet dataSet, string filename, int fileType)
+        public static void GenerateExcelFromDataTable(string sheetName,DataSet dataSet, string filename, int fileType, string headerName)
         {
             switch (fileType)
             {
@@ -48,8 +48,15 @@ namespace DataCollection.Models
                         {
                             for (int i = 0; i < dataSet.Tables.Count; i++)
                             {
+                                var dt = dataSet.Tables[i];
                                 var sName = (string.IsNullOrWhiteSpace(sheetName) ? "Sheet" : sheetName) + (dataSet.Tables.Count > 1 ? (i + 1).ToString() : "");
-                                wb.Worksheets.Add(dataSet.Tables[i], sName);
+                                var ws = wb.Worksheets.Add(sName);
+
+                                ws.Cell(1, 1).Value = headerName;
+                                var rng = ws.Range(1, 1, 1, dt.Columns.Count > 3 ? dt.Columns.Count : 3).Merge();
+                                rng.Style.Font.SetBold(true);
+                                ws.Cell(3, 1).InsertTable(dt);
+                                ws.Columns().AdjustToContents();
                             }
 
                             MemoryStream stream = new MemoryStream();
@@ -60,6 +67,8 @@ namespace DataCollection.Models
                 case 2:
                     {
                         StringBuilder sb = new StringBuilder();
+                        sb.AppendLine(headerName);
+                        sb.AppendLine();
                         for (int i = 0; i < dataSet.Tables.Count; i++)
                         {
                             sb = DataTableToCSV(dataSet.Tables[i], ',', sb);
@@ -70,7 +79,6 @@ namespace DataCollection.Models
                             byte[] info = new UTF8Encoding(true).GetBytes(sb.ToString());
                             fs.Write(info, 0, info.Length);
                         }
-                        
                     }
                     break;
             }            
@@ -94,7 +102,7 @@ namespace DataCollection.Models
                     else
                     {
                         var fileName = item.menuId + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + (result.reportType == 1 ? ".xlsx" : ".csv");
-                        GenerateExcelFromDataTable(item.menuId, data, Path.Combine(dirPath, fileName), result.reportType);
+                        GenerateExcelFromDataTable(item.menuId, data, Path.Combine(dirPath, fileName), result.reportType, GetHeaderName(item.menuId));
                         result.fileName = fileName;
                         result.success = true;
                     }
@@ -119,6 +127,36 @@ namespace DataCollection.Models
             FormsRepository formsRepository = new FormsRepository();
             DataSet dataSet = formsRepository.GetReportData(data.yearMonth, data.menuId, data.deptId);
             return dataSet;
+        }
+
+        private string GetHeaderName(string menuId)
+        {
+            string title = string.Empty;
+            switch (menuId)
+            {
+                case "ADIR":
+                    title = "ADIR DATA PROFORMA FOR RANKING";
+                    break;
+                case "DOAA":
+                    title = "DOAA DATA PROFORMA FOR RANKING";
+                    break;
+                case "DOFA":
+                    title = "DOFA DATA PROFORMA FOR RANKING";
+                    break;
+                case "DOSW":
+                    title = "DOSW DATA PROFORMA FOR RANKING";
+                    break;
+                case "LIBFORM":
+                    title = "LIBRARY DATA PROFORMA FOR RANKING";
+                    break;
+                case "SRICFA":
+                    title = "SRICFA (Funding Agency wise)";
+                    break;
+                case "SRIC":
+                    title = "SRIC DATA PROFORMA FOR RANKING";
+                    break;
+            }
+            return title;
         }
     }
 
